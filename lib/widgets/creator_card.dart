2b0c178b5010980714630acd9110fbf817320f68
Pyveslefebvre <pyveslefebvre@gmail.com>
2024-03-28 19:08:30 +0100

    Affichage icons + liens (voir pour prendre meilleurs qualité)

--- lib/widgets/creator_card.dart
+++ lib/widgets/creator_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart'; // Import the url_launcher package
 
 
 class CreatorCard extends StatelessWidget {
@@ -56,44 +57,73 @@ class CreatorCard extends StatelessWidget {
                     color: backgroundColor.withOpacity(0.7),
                     child: Container(
                       width: 450,
-                      height: 200,
-                      margin: const EdgeInsets.all(8.0),
-                      child: Column( // Modifier pour utiliser Column au lieu de Row
+                      height: 160,
+                      margin: const EdgeInsets.all(10.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Row(
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: Colors.transparent,
-                                radius: 70.0,
-                                child: ClipOval(
-                                  child: Image.network(
-                                    imageUrl,
-                                    fit: BoxFit.cover,
-                                    width: 160.0,
-                                    height: 160.0,
-                                  ),
-                                ),
+                          CircleAvatar(
+                            backgroundColor: Colors.transparent,
+                            radius: 80.0,
+                            child: ClipOval(
+                              child: Image.network(
+                                imageUrl,
+                                fit: BoxFit.cover,
+                                width: 160.0,
+                                height: 160.0,
                               ),
-                              Expanded(
-                                child: Text(
-                                  creatorName,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.bold,
-                                    color: textColor,
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
-                          const SizedBox(height: 10), // Ajoute un espacement entre la photo de profil et les icônes des réseaux sociaux
-                          SocialMediaIcons(
-                            youtubeLink: youtubeLink,
-                            instagramLink: instagramLink,
-                            facebookLink: facebookLink,
-                            tiktokLink: tiktokLink,
-                            twitterLink: twitterLink,
+                          SizedBox(width: 10), // Ajoute un espace entre l'avatar et le texte
+                          Expanded(
+                            child:Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      creatorName.length > 16 ? // Vérifier si le nom a plus de 16 caractères
+                                      Text(
+                                        creatorName,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold,
+                                          color: textColor,
+                                        ),
+                                      ) : // Si le nom a plus de 16 caractères
+                                      Expanded( // Centrer le texte
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            creatorName,
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontSize: 24,
+                                              fontWeight: FontWeight.bold,
+                                              color: textColor,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 2.0), // Hauteur constante au-dessus des icônes
+                                  child: SocialMediaIcons(
+                                    youtubeLink: youtubeLink,
+                                    instagramLink: instagramLink,
+                                    facebookLink: facebookLink,
+                                    tiktokLink: tiktokLink,
+                                    twitterLink: twitterLink,
+                                  ),
+                                ),
+                                SizedBox(height: 2.0), // Espace supplémentaire entre les icônes et le bas de la carte
+                              ],
+                            ),
+
                           ),
                         ],
                       ),
@@ -103,6 +133,7 @@ class CreatorCard extends StatelessWidget {
               ),
             ),
           ),
+
           SizedBox(height: 4),
           Padding(
             padding: const EdgeInsets.only(bottom: 10.0),
@@ -118,7 +149,7 @@ class CreatorCard extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      SizedBox(height: 5),
+                      //SizedBox(height: 5),
                       Card(
                         elevation: 2.0,
                         color: backgroundColor.withOpacity(0.7),
@@ -164,7 +195,7 @@ class CreatorCard extends StatelessWidget {
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Text(
-                              "Description: $description",
+                              description,
                               textAlign: TextAlign.justify,
                               style: TextStyle(fontSize: 14, color: textColor),
                             ),
@@ -195,6 +226,7 @@ class CreatorCard extends StatelessWidget {
   }
 }
 
+
 class SocialMediaIcons extends StatelessWidget {
   final String? youtubeLink;
   final String? instagramLink;
@@ -212,29 +244,68 @@ class SocialMediaIcons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Créer une liste pour stocker les boutons et les SizedBox
+    List<Widget> socialButtons = [];
+    socialButtons.add(SizedBox(width: 10));
+
+    // Ajouter le bouton Instagram et SizedBox d'espacement si le lien est présent
+    if (instagramLink != "None") {
+      socialButtons.add(_buildSocialImageButton('../../assets/instagram.png', instagramLink));
+      socialButtons.add(SizedBox(width: 10));
+    }
+
+    // Ajouter le bouton Facebook et SizedBox d'espacement si le lien est présent
+    if (facebookLink != "None") {
+      socialButtons.add(_buildSocialImageButton('../../assets/facebook.png', facebookLink));
+      socialButtons.add(SizedBox(width: 10));
+    }
+
+    // Ajouter le bouton TikTok et SizedBox d'espacement si le lien est présent
+    if (tiktokLink != "None") {
+      socialButtons.add(_buildSocialImageButton('../../assets/tiktok.png', tiktokLink));
+      socialButtons.add(SizedBox(width: 10));
+    }
+
+    // Ajouter le bouton Twitter et SizedBox d'espacement si le lien est présent
+    if (twitterLink != "None") {
+      socialButtons.add(_buildSocialImageButton('../../assets/twitter.png', twitterLink));
+      socialButtons.add(SizedBox(width: 10));
+    }
+
+    // Retirer le dernier SizedBox car il n'est pas nécessaire après le dernier bouton
+    if (socialButtons.isNotEmpty) {
+      socialButtons.removeLast();
+    }
+
+    // Retourner la rangée contenant les boutons sociaux
     return Row(
-      children: <Widget>[
-        _buildSocialIconButton(Icons.Instagram, instagramLink),
-        _buildSocialIconButton(Icons.facebook, facebookLink),
-        _buildSocialIconButton(Icons.tiktok, tiktokLink),
-        _buildSocialIconButton(Icons.X, twitterLink),
-      ],
+      children: socialButtons,
     );
   }
 
-  Widget _buildSocialIconButton(IconData iconData, String? link) {
-    return link != 'None'
-        ? IconButton(
-      icon: Icon(iconData),
-      onPressed: () {
-        // Handle the tap event here
-        if (link != 'None') {
-          // Open the link, you can use packages like url_launcher for this
-          // Example:
-          // launch(link);
-        }
+
+
+  Widget _buildSocialImageButton(String imagePath, String? link) {
+    return link != "None"
+        ? InkWell(
+      onTap: () {
+        _launchURL(link!); // Launch the provided link
       },
+      child: Image.asset(
+        imagePath,
+        width: 30,
+        height: 30,
+      ),
     )
         : const SizedBox(); // Return an empty container if the link is null
+  }
+
+  // Function to launch the provided URL
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
