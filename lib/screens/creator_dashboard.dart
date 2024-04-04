89cb5249d10f3138525aa27233eb01b66861ef28
virgile <virgile.louvel@gmail.com>
2024-04-04 17:19:17 +0200

    ajout graphique, modif couleur, pas mal de modif a faire encore... + test fetch avec la search bar

--- lib/screens/creator_dashboard.dart
+++ lib/screens/creator_dashboard.dart
@@ -124,6 +124,7 @@ class _CreatorDashboardState extends State<CreatorDashboard> {
                           ),
                   ],
                 )
+              )
             ],
           ),
         ),
