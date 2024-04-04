9d4045e49345d948c018af0c0dfa309fa5b1f7d8
virgile <virgile.louvel@gmail.com>
2024-04-04 23:00:26 +0200

    tentative couleur + barre de chargement (elle est infini pour l'instant)

--- lib/screens/creator_dashboard.dart
+++ lib/screens/creator_dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_front/models/view_info.dart';
+import '../main.dart';
 import '../models/creator_info.dart';
 import '../models/post_info.dart';
 import '../services/youtube_api_service.dart';
@@ -17,10 +18,26 @@ class CreatorDashboard extends StatefulWidget {
 class _CreatorDashboardState extends State<CreatorDashboard> {
   final YoutubeApiService _apiService = YoutubeApiService();
   final TextEditingController _controller = TextEditingController();
+  final colorSchemeSeed = ColorScheme.fromSeed(seedColor: Colors.blueAccent);
   CreatorInfo? _creatorInfo;
   InfoChart? _infoChart;
-  ColorScheme? colorScheme;
   List<PostInfo> _latestPosts = [];
+  late ColorScheme  currentColorScheme;
+  late bool isLoading;
+
+  @override
+  void initState(){
+    super.initState();
+    currentColorScheme = colorSchemeSeed;
+    isLoading=false;
+  }
+
+  Future<void> _updateImage(String provider) async {
+    final ColorScheme newColorScheme = await ColorScheme.fromImageProvider(provider: NetworkImage(_creatorInfo!.channelProfilePicLink));
+    setState(() {
+      currentColorScheme = newColorScheme;
+    });
+  }
 
   void _fetchCreatorAndLatestPosts(String channelName) async {
     try {
@@ -28,20 +45,25 @@ class _CreatorDashboardState extends State<CreatorDashboard> {
       final channelId = channelInfo.channelId;
       final posts = await _apiService.fetchLatestPosts(channelId);
 
-      setState(() {
+          setState(() {
         _creatorInfo = channelInfo;
         _latestPosts = posts;
-      });
+        isLoading= true;
+
+          });
+      if (_creatorInfo != null) {
+        _updateImage(_creatorInfo!.channelProfilePicLink);
+      }
     } catch (e) {
       print(e);
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
-          title: Text('Error'),
-          content: Text('Failed to fetch data. Please try again later.'),
+          title: const Text('Error'),
+          content: const Text('Failed to fetch data. Please try again later.'),
           actions: [
             TextButton(
-              child: Text('OK'),
+              child: const Text('OK'),
               onPressed: () => Navigator.of(context).pop(),
             ),
           ],
@@ -50,18 +72,10 @@ class _CreatorDashboardState extends State<CreatorDashboard> {
     }
   }
 
-  void _updatePaletteGenerator(String imageUrl) async {
-    final ColorScheme newcolorScheme = await ColorScheme.fromImageProvider(provider: NetworkImage(imageUrl),
-      // ... Other properties
-
-    );
-    setState(() {
-      colorScheme = newcolorScheme;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+      final ColorScheme colorScheme = currentColorScheme;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
@@ -71,64 +85,64 @@ class _CreatorDashboardState extends State<CreatorDashboard> {
         ),
       ),
       body: Align(
-        alignment: Alignment.topLeft,
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 1100),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          alignment: Alignment.topLeft,
+        child: isLoading
+          ? const LinearProgressIndicator()
+          : Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    TextFields(onSearch: _fetchCreatorAndLatestPosts),
-                    if (_creatorInfo != null)
-                      Padding(
-                        padding: const EdgeInsets.only(left:10.0,bottom: 8.0,right: 30.0),
-                        child: CreatorCard(
-                          creatorName: _creatorInfo!.channelName,
-                          subscribers: _creatorInfo!.subscriberCount,
-                          views: _creatorInfo!.viewCount,
-                          videos: _creatorInfo!.videoCount,
-                          description: _creatorInfo!.channelDescription,
-                          imageUrl: _creatorInfo!.channelProfilePicLink,
-                          youtubeLink: _creatorInfo!.youtubeLink,
-                          instagramLink: _creatorInfo!.instagramLink,
-                          tiktokLink: _creatorInfo!.tiktokLink,
-                          twitterLink: _creatorInfo!.twitterLink,
-                          backgroundColor: Theme.of(context).colorScheme.background,
-                        ),
-                      ),
-                  ],
+              TextFields(onSearch: _fetchCreatorAndLatestPosts),
+              if (_creatorInfo != null)
+                Padding(
+                  padding: const EdgeInsets.only(left:10.0,bottom: 8.0,right: 30.0),
+                  child: CreatorCard(
+                    creatorName: _creatorInfo!.channelName,
+                    subscribers: _creatorInfo!.subscriberCount,
+                    views: _creatorInfo!.viewCount,
+                    videos: _creatorInfo!.videoCount,
+                    description: _creatorInfo!.channelDescription,
+                    imageUrl: _creatorInfo!.channelProfilePicLink,
+                    youtubeLink: _creatorInfo!.youtubeLink,
+                    instagramLink: _creatorInfo!.instagramLink,
+                    tiktokLink: _creatorInfo!.tiktokLink,
+                    twitterLink: _creatorInfo!.twitterLink,
+                    backgroundColor: Theme.of(context).colorScheme.background,
+                  ),
                 ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _latestPosts.length,
-                  itemBuilder: (context, index) {
-                    return PostCard(postInfo: _latestPosts[index]);
-                  },
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Padding(
-                        padding: EdgeInsets.all(16.0)
-                    ),
-                    if (_creatorInfo != null)
-                      ViewersChart(
-                        baseColor: Theme.of(context).colorScheme.onPrimary,
-                        chartInfo: InfoChart(_creatorInfo!.channelName),
-                          ),
-                  ],
-                )
-              )
             ],
           ),
         ),
-      ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _latestPosts.length,
+            itemBuilder: (context, index) {
+              return PostCard(postInfo: _latestPosts[index]);
+            },
+          ),
+        ),
+        Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                    padding: EdgeInsets.all(16.0)
+                ),
+                if (_creatorInfo != null)
+                  ViewersChart(
+                    baseColor: Theme.of(context).colorScheme.onPrimary,
+                    chartInfo: InfoChart(_creatorInfo!.channelName),
+                  ),
+              ],
+            )
+        )
+      ],
+    ),
+    ),
     );
   }
 }
+
